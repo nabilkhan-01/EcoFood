8b6726adf3965f63d7eedf2436bd88de81b8e12b
Nabil Khan <89152492+Dynamickhan@users.noreply.github.com>
2024-12-21 03:25:12 +0530

    .

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,6 @@ const Header = () => {
     <div className='header'>
       <div className='header-contents'>
         <h2>Order your favourite food</h2>
-        <p>Discover a handpicked array of delectable dishes, crafted with top-quality ingredients and culinary expertise. We aim to captivate your palate and create memorable dining experiences, one savory bite at a time.</p>
       </div>
     </div>
   )
