98ca4b08e6e1c58cd7b7fb531f67837ae15ac500
Nabil Khan <89152492+Dynamickhan@users.noreply.github.com>
2024-12-21 01:04:25 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
@@ -8,17 +8,15 @@ import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <Router basename="/EcoFood"> {/* Add this line to handle the subdirectory */}
-      <div className="app">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/order" element={<PlaceOrder />} />
-        </Routes>
-      </div>
+    <div className="app">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
       <Footer />
-    </Router>
+    </div>
   );
 };
 
